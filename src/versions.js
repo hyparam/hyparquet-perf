4b9c0f7ee1c098ed0539e8a9b1321e2f4c20b3d7
Sylvain Lesage <severo@rednegra.net>
2026-03-11 16:08:57 +0100

    add tests and run on versions >=0.20

--- src/versions.js
+++ src/versions.js
@@ -1,20 +1,25 @@
 import { promises as fs } from 'fs'
-import { appDir, streamCommand , median} from './utils.js'
+import { appDir, streamCommand } from './utils.js'
 
 async function run() {
   // Fetch the hightable versions from npm
   const versionsData = await fetch('https://registry.npmjs.org/hightable')
     .then(res => res.json())
 
-  // Extract the version numbers and reverse() for the newest first
-  const versions = Object.keys(versionsData.versions).reverse()
-    .filter(v => v.startsWith('0.26.')) // Only test versions starting with 0.26. for now.
+  const versions = Object.keys(versionsData.versions).reverse().map(version => {
+    const [major, minor, patch] = version.split('.')
+    return {
+      version,
+      major: parseInt(major),
+      minor: parseInt(minor),
+      patch: parseInt(patch)
+    }
+  }).filter(({ minor }) => minor >= 20) // Only test versions with minor >= 20 for now.
 
   // Remove perf.jsonl
   await fs.unlink('perf.jsonl').catch(() => {})
 
-
-  for (const version of versions) {
+  for (const { version } of versions) {
     console.log(`\nRunning tests for hightable@${version}`)
 
     // Install the specified version (streams stdout/stderr)
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,30 +3,57 @@ import { test, expect } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
   const client = await page.context().newCDPSession(page);
 
-  // Throttle the network, to have stable conditions for performance testing.
-  await client.send('Network.overrideNetworkState', {
-    offline: false, // must be online to get the page
-    downloadThroughput: 400 * 1024 / 8, // 400 kbps
-    uploadThroughput: 400 * 1024 / 8, // 400 kbps
-    latency: 400, // 400 ms latency
-  });
+  // No need to throttle the network (Network.overrideNetworkState)
 
   // Throttle the CPU, to avoid performance differences being hidden by a too fast CPU.
-  await client.send('Emulation.setCPUThrottlingRate', { rate: 10 }); // 10x slower
-
-  await page.goto('http://localhost:3000');
+  await client.send('Emulation.setCPUThrottlingRate', { rate: 4 }); // 4x slower
 })
 
-test('has title', async ({ page }) => {
-  await test.step('check title', async () => {
-    await expect(page).toHaveTitle(/HighTable/);
-  })
-});
-
-test('has cell content', async ({ page }) => {
-  for (let i=0; i<100; i++) {
+test('load first cell', async ({ page }) => {
+  for (let i = 0; i < 20; i++) {
+    await page.goto('http://localhost:3000');
     await test.step('first-cell', async () => {
       await expect(page.getByRole('cell', { name: `Name0` })).toBeVisible();
+    })
+  }
+});
+
+test('scroll by one row', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page.getByRole('cell', { name: `Name0` })).toBeVisible();
+  const scroller = page.getByRole('group')
+
+  for (let i = 0; i < 20; i++) {
+    await test.step('scrolled', async () => {
+      await scroller.evaluate(e => e.scrollTop += 33);
+      await expect(page.getByRole('cell', { name: `Name${i}`, exact: true })).toBeVisible();
+    })
+  }
+});
+
+test('scroll by 10 rows', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page.getByRole('cell', { name: `Name0` })).toBeVisible();
+  const scroller = page.getByRole('group')
+
+  for (let i = 0; i < 20; i++) {
+    await test.step('scrolled', async () => {
+      await scroller.evaluate(e => e.scrollTop += 330);
+      await expect(page.getByRole('cell', { name: `Name${i * 10}`, exact: true })).toBeVisible();
+    })
+  }
+});
+
+test('sort', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page.getByRole('cell', { name: `Name0` })).toBeVisible();
+  const header = page.getByRole('columnheader', { name: 'Name' });
+
+  for (let i = 0; i < 20; i++) {
+    const visibleCell = i%3 === 0 ? "Name1000": i%3 === 1 ? "Name9999" : "Name2"
+    await test.step('sorted', async () => {
+      await header.click();
+      await expect(page.getByRole('cell', { name: visibleCell, exact: true })).toBeVisible();
     })
   }
 });
